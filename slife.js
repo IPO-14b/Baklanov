861031d47732ef7830009ef05120ddbca7da3f27
nick010001110 <a.i.001@mail.ru>
2017-06-11 18:51:53 +0300

    добавлены протитипы функции

--- slife.js
+++ slife.js
@@ -6,3 +6,27 @@ var scr_0 = '#050505';
 var scr_1 = '#41a91d';
 var active = 0;
 var cash;
+function LoadLife(first) {
+   
+}function TableBuild() {
+   
+}function InitCash() {
+   
+}function CellTrigger(x, y) {
+   
+}function InitSpace(x, y) {
+   }function SwapLayers() {
+   
+}function CalcNextStep() {
+   
+}function PlayLife(check) {
+   
+}function StopLife() {
+ 
+}function SetOptions() {
+   
+}function PrintAge() {
+ 
+}function DumpOptions() {
+  
+}
